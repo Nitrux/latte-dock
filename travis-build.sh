baae05703f91a437f0655cc230d5e327d68eaf05
Uri Herrera <hello@uriherrera.com>
2021-06-11 22:46:23 -0500

    create pkg

---
+++ travis-build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+apt -qq update
+apt -qq -yy install equivs curl git wget gnupg2
+
+### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
+
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	55751E5D > /dev/null
+
+apt -qq update
+
+### Install Dependencies
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts lintian build-essential automake autotools-dev
+mk-build-deps -i -t "apt-get --yes" -r
+
+### Clone repo.
+
+git clone https://github.com/KDE/latte-dock.git
+
+
+### Build Deb
+
+mkdir source
+mv ./* source/ # Hack for debuild
+cd source
+debuild -b -uc -us
