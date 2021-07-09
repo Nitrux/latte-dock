ec34a071db955c7c4388bbefedfb10913dfb9c38
Uri Herrera <hello@uriherrera.com>
2021-07-09 13:18:43 -0500

    update pkg

--- travis-build.sh
+++ travis-build.sh
@@ -16,7 +16,7 @@ apt -qq update
 
 ### Install Dependencies
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream
 
 mk-build-deps -i -t "apt-get --yes" -r
 
