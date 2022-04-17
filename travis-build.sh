7bcc6edb1187576a72eb018caca674a11547f52f
Uri Herrera <hello@uriherrera.com>
2022-04-17 18:43:03 -0500

    add build dep

--- travis-build.sh
+++ travis-build.sh
@@ -54,7 +54,6 @@ DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
 	qml-module-qtgraphicaleffects \
 	qml-module-qtquick-controls \
 	qml-module-qtquick-shapes \
-	qml-module-qtquick-controls-styles-breeze \
 	plasma-workspace-dev \
 	qtbase5-dev \
 	qtdeclarative5-dev \
