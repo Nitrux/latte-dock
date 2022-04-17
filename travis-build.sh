4099156859a9d3dd333a6c6f59f4d74a9bd76ce5
Uri Herrera <hello@uriherrera.com>
2022-04-17 05:57:21 -0500

    use checkinstall

--- travis-build.sh
+++ travis-build.sh
@@ -2,35 +2,108 @@
 
 set -x
 
-apt -qq update
-apt -qq -yy install equivs curl git wget gnupg2
+### Install Build Tools #1
 
-### FIXME - the container mauikit/ubuntu-18.04-amd64 does have the neon repo but for some idiotic reason it isn't working here
+DEBIAN_FRONTEND=noninteractive apt -qq update
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	appstream \
+	automake \
+	autotools-dev \
+	build-essential \
+	checkinstall \
+	cmake \
+	curl \
+	devscripts \
+	equivs \
+	extra-cmake-modules \
+	gettext \
+	git \
+	gnupg2 \
+	lintian \
+	wget
+
+### Add Neon Sources
 
 wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	55751E5D > /dev/null
 
-apt -qq update
+DEBIAN_FRONTEND=noninteractive apt -qq update
 
-### Install Dependencies
+### Install Package Build Dependencies #1
 
-DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends devscripts debhelper gettext lintian build-essential automake autotools-dev cmake extra-cmake-modules appstream
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	libkf5i18n-dev \
+	libkf5kio-dev \
+	libkf5notifications-dev \
+	libkf5solid-dev \
+	libkf5syntaxhighlighting-dev \
+	libqt5svg5-dev \
+	libqt5x11extras5-dev \
+	libxcb-icccm4-dev \
+	libxcb-shape0-dev \
+	qml-module-qtgraphicaleffects \
+	qml-module-qtquick-controls2 \
+	qml-module-qtquick-shapes \
+	qtbase5-dev \
+	qtdeclarative5-dev \
+	qtquickcontrols2-5-dev
 
-mk-build-deps -i -t "apt-get --yes" -r
+### Clone Repository
 
-### Clone repo.
+git clone --depth 1 --branch v0.10 https://github.com/KDE/latte-dock.git
 
-git clone https://github.com/KDE/latte-dock.git
+rm -rf latte-dock/{CHANGELOG.md,LICENSES,README.md,INSTALLATION.md,NEWFEATURES.md,logo.png}
 
-mv latte-dock/* .
+### Compile Source
 
-rm -rf latte-dock LICENSES README.md
+mkdir -p latte-dock//build && cd latte-dock//build
 
-### Build Deb
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
+	-DQUICK_COMPILER=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ..
 
-mkdir source
-mv ./* source/ # Hack for debuild
-cd source
-debuild -b -uc -us
+make
+
+### Run checkinstall and Build Debian Package
+### DO NOT USE debuild, screw it
+
+>> description-pak printf "%s\n" \
+	'Dock based on plasma frameworks.' \
+	'' \
+	'Latte is a dock based on plasma frameworks that provides an elegant and' \
+	'intuitive experience for your tasks and plasmoids.' \
+	'' \
+	'It animates its contents by using parabolic zoom effect and tries to be there only when' \
+	'it is needed.' \
+	''
+
+checkinstall -D -y \
+	--install=no \
+	--fstrans=yes \
+	--pkgname=latte-dock \
+	--pkgversion=0.10.801+nitrux+git \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=lib \
+	--pkgsource=latte-dock \
+	--pakdir=../.. \
+	--maintainer=uri_herrera@nxos.org \
+	--provides=latte-dock \
+	--requires=libc6,libkf5configcore5,libkf5coreaddons5,libkf5i18n5,libkf5notifications5,libqt5core5a,libqt5gui5,libqt5qml5,libstdc++6,qml-module-org-kde-kirigami2,qml-module-qtquick-controls2,qml-module-qtquick-shapes \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes \
+	--deldesc=yes
