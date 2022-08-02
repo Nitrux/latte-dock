24ef61a85ba81add74a6603b386fe5b105b4bcec
Uri Herrera <hello@uriherrera.com>
2022-08-02 17:02:14 -0500

    fix deb pkg path

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -72,7 +72,7 @@ checkinstall -D -y \
 	--pkglicense=LGPL-3 \
 	--pkggroup=lib \
 	--pkgsource=latte-dock \
-	--pakdir=../.. \
+	--pakdir=. \
 	--maintainer=uri_herrera@nxos.org \
 	--provides=latte-dock \
 	--requires=libc6,libkf5configcore5,libkf5coreaddons5,libkf5i18n5,libkf5notifications5,libqt5core5a,libqt5gui5,libqt5qml5,libstdc++6,libsm6,qml-module-org-kde-kirigami2,qml-module-qtquick-controls2,qml-module-qtquick-shapes \
